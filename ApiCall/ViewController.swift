ab6a952a912435431d6ea79d851408c7672cf989
Andrew Ananda <andrewtiema@gmail.com>
2020-12-17 14:33:00 +0300

    Initial Commit

---
+++ ApiCall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApiCall
+//
+//  Created by Johnstone Ananda on 17/12/2020.
+//  Copyright © 2020 Andrew Ananda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
